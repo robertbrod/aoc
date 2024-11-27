3974dee26265d4b048101433d3723d2cd7a849cd
Robert Brod <robbrod93@gmail.com>
2024-11-26 21:54:06 -0600

    Progress towards daily puzzle scaffolding

---
+++ 2023/1/solution.py
@@ -0,0 +1,5 @@
+def solve_part_one(input):
+    return 0
+
+def solve_part_two(input):
+    return 0
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import aoc_gateway
+import importlib
 import util
 import os
 
@@ -15,11 +16,19 @@ if not os.path.exists(f"{YEAR}/"):
 else:
     print("Skipping yearly helper for creating directories; it should already exist!")
 
-# ---------- Puzzle input ----------
+# ---------- Puzzle ----------
 
 try:
     # Fetch input data from Advent of Code website and cache on disk
     data = aoc_gateway.fetch_input(YEAR, DAY)
     util.cache_input_data(data, YEAR, DAY)
+    
+    module_name = f"{YEAR}.{DAY}.solution"
+    solution_module = importlib.import_module(module_name)
+
+    part_one_result = solution_module.solve_part_one(data)
+    part_two_result = solution_module.solve_part_two(data)
+except ModuleNotFoundError:
+    print(f"Solution for {YEAR} day {DAY} not found")
 except Exception as error:
     print(error)
