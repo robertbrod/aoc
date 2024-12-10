3c1ce2f5462798e912363d3017f63626dd21a1e6
Robert Brod <rbrod@capsher.com>
2024-12-10 13:14:17 -0600

    Day 10

--- 2024/10/solution.py
+++ 2024/10/solution.py
@@ -45,7 +45,28 @@ def fetch_neigbors(topographic_map, coords):
 
     return neighbors
 
-def run_bfs(topographic_map, trailhead):
+def run_bfs_part1(topographic_map, trailhead):
+    queue = deque([(trailhead[0], trailhead[1], 0)])
+    visited = set()
+    visited.add((trailhead[0], trailhead[1], 0))
+    trailhead_endings = set()
+
+    while queue:
+        x, y, val = queue.popleft()
+
+        if val == 9:
+            trailhead_endings.add((x, y))
+            continue
+
+        neighbors = fetch_neigbors(topographic_map, (x, y))
+        for neighbor in neighbors:
+            if topographic_map[neighbor[0]][neighbor[1]] == val + 1:
+                visited.add((neighbor[0], neighbor[1], val + 1))
+                queue.append((neighbor[0], neighbor[1], val + 1))
+
+    return len(trailhead_endings)
+
+def run_bfs_part2(topographic_map, trailhead):
     trailhead_score = 0
     queue = deque([(trailhead[0], trailhead[1], 0)])
     visited = set()
@@ -56,7 +77,7 @@ def run_bfs(topographic_map, trailhead):
 
         if val == 9:
             trailhead_score += 1
-            break
+            continue
 
         neighbors = fetch_neigbors(topographic_map, (x, y))
         for neighbor in neighbors:
@@ -72,9 +93,16 @@ def solve_part_one(input):
     topographic_map, trailheads = parse_input(input)
 
     for trailhead in trailheads:
-        total_trailhead_score += run_bfs(topographic_map, trailhead)
+        total_trailhead_score += run_bfs_part1(topographic_map, trailhead)
 
     return total_trailhead_score
 
 def solve_part_two(input):
-    return 0
+    total_trailhead_score = 0
+    
+    topographic_map, trailheads = parse_input(input)
+
+    for trailhead in trailheads:
+        total_trailhead_score += run_bfs_part2(topographic_map, trailhead)
+
+    return total_trailhead_score
