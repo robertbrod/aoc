4387da1bb2278a41b42f512db7c5ccbd192ae9c7
Robert Brod <rbrod@capsher.com>
2024-12-10 13:04:00 -0600

    Day 10 progress

--- 2024/10/solution.py
+++ 2024/10/solution.py
@@ -1,7 +1,80 @@
 # Advent of Code 2024 - Day 10
 
+import util
+from collections import deque
+
+def parse_input(input):
+    topographic_map = [] 
+    trailheads = set()
+
+    for x, row in enumerate(input):
+        topographic_map.append([])
+        for y, char in enumerate(row):
+            digit = int(char)
+            if digit == 0:
+                trailheads.add((x, y))
+            topographic_map[x].append(digit)
+
+    return topographic_map, trailheads
+
+def fetch_neigbors(topographic_map, coords):
+    neighbors = set()
+    width = len(topographic_map[0])
+    height = len(topographic_map)
+    x, y = coords[0], coords[1]
+
+    # Up
+    dx, dy = 0, -1
+    if util.in_bounds_2d(x + dx, y + dy, width, height):
+        neighbors.add((x + dx, y + dy))
+
+    # Down
+    dx, dy = 0, 1 
+    if util.in_bounds_2d(x + dx, y + dy, width, height):
+        neighbors.add((x + dx, y + dy))
+
+    # Left
+    dx, dy = -1, 0
+    if util.in_bounds_2d(x + dx, y + dy, width, height):
+        neighbors.add((x + dx, y + dy))
+
+    # Right
+    dx, dy = 1, 0
+    if util.in_bounds_2d(x + dx, y + dy, width, height):
+        neighbors.add((x + dx, y + dy))
+
+    return neighbors
+
+def run_bfs(topographic_map, trailhead):
+    trailhead_score = 0
+    queue = deque([(trailhead[0], trailhead[1], 0)])
+    visited = set()
+    visited.add((trailhead[0], trailhead[1], 0))
+
+    while queue:
+        x, y, val = queue.popleft()
+
+        if val == 9:
+            trailhead_score += 1
+            break
+
+        neighbors = fetch_neigbors(topographic_map, (x, y))
+        for neighbor in neighbors:
+            if topographic_map[neighbor[0]][neighbor[1]] == val + 1:
+                visited.add((neighbor[0], neighbor[1], val + 1))
+                queue.append((neighbor[0], neighbor[1], val + 1))
+
+    return trailhead_score
+
 def solve_part_one(input):
-    return 0
+    total_trailhead_score = 0
+    
+    topographic_map, trailheads = parse_input(input)
+
+    for trailhead in trailheads:
+        total_trailhead_score += run_bfs(topographic_map, trailhead)
+
+    return total_trailhead_score
 
 def solve_part_two(input):
     return 0
--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def run_solution():
     except ModuleNotFoundError:
         print(f"Solution for {year} day {day} not found")
     
-    input_data = util.fetch_input(year, day).splitlines()
+    input_data = util.fetch_sample_input(year, day).splitlines()
     
     if part == 1:
         start_time = time.perf_counter()
@@ -44,8 +44,8 @@ def run_solution():
         execution_time = end_time - start_time
         print(f"Solution execution time: {execution_time * 1000:.3f} ms. Computed answer: {result}")
         
-        response = aoc_gateway.submit_answer(year, day, part, result)
-        print(response)
+        # response = aoc_gateway.submit_answer(year, day, part, result)
+        # print(response)
     elif part == 2:
         start_time = time.perf_counter()
         result = solution_module.solve_part_two(input_data)
