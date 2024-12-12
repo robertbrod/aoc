cfba05ca91223576a121c7e6a68d2987ed7509a1
Robert Brod <rbrod@capsher.com>
2024-12-12 17:30:49 -0600

    Day 12 minor progress

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -1,6 +1,11 @@
 # Advent of Code 2024 - Day 12
 
+def parse_input(input):
+    return [list(line) for line in input]
+
 def solve_part_one(input):
+    garden_map = parse_input(input)
+
     return None
 
 def solve_part_two(input):
