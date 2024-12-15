c19fa1c13e3876d1341e1483fd0a84af515427f0
Robert Brod <robbrod93@gmail.com>
2024-12-14 22:35:02 -0600

    Day 14!

--- 2024/14/solution.py
+++ 2024/14/solution.py
@@ -20,18 +20,65 @@ def parse_input(input):
     return robots
 
 def move_robots(robots, quadrants, seconds, width, height):
+    mid_width = width // 2
+    mid_height = height // 2
+    
     for robot in robots:
-5        x_init, y_init = robot.position
+        x, y = robot.position
         x_velocity, y_velocity = robot.velocity
 
-        x_final = (x_init + x_velocity * seconds)
-        y_final = (y_init + y_velocity * seconds)
+        x = (x + x_velocity * seconds) % width
+        y = (y + y_velocity * seconds) % height
 
-        x_final %= width
-        y_final %= height
+        if x == mid_width or y == mid_height:
+            continue
+        
+        # north-west
+        elif x < mid_width and y < mid_height:
+             quadrants["north-west"] += 1
+        
+        # north-east 
+        elif x > mid_width and y < mid_height:
+            quadrants["north-east"] += 1
+            
+        # south-west    
+        elif x < mid_width and y > mid_height:
+            quadrants["south-west"] += 1
+            
+        # south-east
+        elif x > mid_width and y > mid_height:
+            quadrants["south-east"] += 1
+            
+def move_and_print_robots(robots, matrix, width, height):
+    density = 0
+    
+    for seconds in range(1, 99999):
+        for robot in robots:
+            x, y = robot.position
+            x_velocity, y_velocity = robot.velocity
 
-        print(f"Final adjusted position: ({x_final}, {y_final})")
-    pass
+            x = (x + x_velocity) % width
+            y = (y + y_velocity) % height
+            
+            matrix[y][x] = '#'
+            
+            robot.position = (x, y)
+            
+            if 35 < x < 70 and 35 < y < 70:
+                density += 1
+                
+        if density > 100:
+            with open("2024/14/bathroom_map.txt", "w") as file:
+                for row in matrix:
+                    file.write("".join(row) + "\n")
+                file.write(f"Time elapsed: {seconds} seconds\n")
+                    
+        density = 0
+        reset_matrix(matrix)
+        
+def reset_matrix(matrix):
+    matrix.clear()
+    matrix.extend([['.'] * 101 for _ in range(103)])
 
 def solve_part_one(input):
     robots = parse_input(input)
@@ -43,10 +90,14 @@ def solve_part_one(input):
         "south-east": 0
     }
 
-    move_robots(robots, quadrants, 100, 11, 7)
+    move_robots(robots, quadrants, 100, 101, 103)
 
     # Compute and return safety factor
-    return None
+    return quadrants["north-west"] * quadrants["north-east"] * quadrants["south-west"] * quadrants["south-east"]
 
 def solve_part_two(input):
+    robots = parse_input(input)
+    matrix = [['.'] * 101 for _ in range(103)]
+    move_and_print_robots(robots, matrix, 101, 103)
+
     return None
