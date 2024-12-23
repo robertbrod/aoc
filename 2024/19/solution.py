b8e80aa9ebb476694f65dca3eeb62765d64dd666
Robert Brod <robbrod93@gmail.com>
2024-12-22 22:09:06 -0600

    Day 19 progress, needs memoization (probably)

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -1,5 +1,7 @@
 # Advent of Code 2024 - Day 19
 
+import functools
+
 def parse_input(input):
     towels, desired_patterns = [], []
     
@@ -11,10 +13,35 @@ def parse_input(input):
             
     return towels, desired_patterns 
 
+def get_potential_next_designs(towels, target):
+    potential_next_designs = []
+    for towel in towels:
+        substring_length = len(towel)
+        if target[:substring_length] == towel:
+            potential_next_designs.append(towel)
+            
+    return potential_next_designs
+
+def is_pattern_possible(towels, desired_pattern, index, current_pattern):
+    if index == len(desired_pattern):
+        return current_pattern == desired_pattern
+    
+    potential_next_designs = get_potential_next_designs(towels, desired_pattern[index:])
+    possible = False
+    for design in potential_next_designs:
+        possible = is_pattern_possible(towels, desired_pattern, index + len(design), current_pattern + design)
+        
+    return possible
+
 def solve_part_one(input):
     towels, desired_patterns = parse_input(input)
     
-    return None
+    possible_designs = 0
+    for desired_pattern in desired_patterns:
+        if is_pattern_possible(towels, desired_pattern, 0, ""):
+            possible_designs += 1
+            
+    return possible_designs
 
 def solve_part_two(input):
     return None
