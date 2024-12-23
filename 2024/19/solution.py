7e8bd87c0d6dd5a4ca6582e62414991410599a9e
Robert Brod <robbrod93@gmail.com>
2024-12-22 21:17:48 -0600

    Day 19 progress

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -1,6 +1,19 @@
 # Advent of Code 2024 - Day 19
 
+def parse_input(input):
+    towels, desired_patterns = [], []
+    
+    for index, line in enumerate(input):
+        if index == 0:
+            towels = line.replace(" ", "").split(',')
+        elif line:
+            desired_patterns.append(line)       
+            
+    return towels, desired_patterns 
+
 def solve_part_one(input):
+    towels, desired_patterns = parse_input(input)
+    
     return None
 
 def solve_part_two(input):
